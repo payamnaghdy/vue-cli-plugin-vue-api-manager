92d7b5622b09d5694c9798fefedbf43ac93ecc3f
Payam Naghdi <p.naghdi@payampardaz.local>
2021-08-14 20:04:56 +0430

    Fix: Set vue-api-manager version in the generator

--- generator/index.js
+++ generator/index.js
@@ -2,7 +2,7 @@ module.exports = function (api, options) {
     const {authTokenStore} = options
     api.extendPackage({
         dependencies: {
-            '@payamnaghdy/vue-api-manager': 'latest'
+            "@payamnaghdy/vue-api-manager": "^0.0.2"
         }
     })
     api.render('./templates', {
