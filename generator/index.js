f3f27092d1117c9f806a92dd02203ef97f78a068
payam naghdi <p.naghdi@payamparda.local>
2021-11-13 17:55:23 +0330

    Use version 9 vue api manager

--- generator/index.js
+++ generator/index.js
@@ -2,7 +2,7 @@ module.exports = function (api, options) {
     const {authTokenStore} = options
     api.extendPackage({
         dependencies: {
-            "@payamnaghdy/vue-api-manager": "^0.0.6"
+            "@payamnaghdy/vue-api-manager": "^0.0.9"
         }
     })
     api.render('./templates', {
